ee8a769ea593493a8f1366e45acaf3abd6d1e8a2
John Peters <jgpeters3@wisc.edu>
2025-08-13 10:43:59 -0500

    channvelvit with config file instead of command line arugments for chtc ease of use

--- channel_vit_dino/chtc_job.sh
+++ channel_vit_dino/chtc_job.sh
@@ -1,22 +1,22 @@
 container_image = file:///staging/groups/caicedo_group/images/channel_vit_dino.sif
-log = train$(Cluster).log
+log = logs/train$(Cluster).log
 universe = container
-executable = execute_job.sh
+executable = execute_sweep.sh
 arguments = $(Process)
-output = train$(Cluster)_$(Process).out
-error = train$(Cluster)_$(Process).err
-environment = "WANDB_API_KEY=$(wandb_key)"
+output = logs/train$(Cluster)_$(Process).out
+error = logs/train$(Cluster)_$(Process).err
+environment = "WANDB_API_KEY=$(wandb_key) CONFIG_NAME=$(config_name)"
 
 # Specify that HTCondor should transfer files to and from the
 #  computer where each job runs. The last of these lines *would* be
 #  used if there were any other files needed for the executable to use.
 should_transfer_files = YES
 when_to_transfer_output = ON_EXIT_OR_EVICT
-transfer_input_files = execute_job.sh, /home/jgpeters3/CHAMMI-75, /hdd/jcaicedo/morphem/dataset/sampling/chammi_train.zip, /hdd/jcaicedo/morphem/dataset/sampling/multi_channel_chammi_metadata.csv
+transfer_input_files = execute_job.sh, /home/jgpeters3/CHAMMI-75, /hdd/jcaicedo/morphem/dataset/sampling/chammi_train.zip, /hdd/jcaicedo/morphem/dataset/sampling/multi_channel_chammi_metadata.csv, $(config_path)
 # Tell HTCondor what amount of compute resources
 #  each job will need on the computer where it runs.
 
-requirements = (Machine == "jcaicedogpu0002.chtc.wisc.edu")
+requirements = ( Machine == "jcaicedogpu0002.chtc.wisc.edu" )
 request_cpus = 8
 request_memory = 400GB
 request_disk =  100GB
---
+++ channel_vit_dino/submit_chtc.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+import subprocess
+import numpy as np
+import sys
+from omegaconf import OmegaConf
+sys.path.append('../../')
+
+from config import *
+
+default_path = '/home/jgpeters3/FoundationModels/chtc/dinov2/configs'
+
+def submit_command(command:str):
+    result = subprocess.run(
+                    command,
+                    shell=True,
+                    # capture_output=True,
+                    check=True,
+                    text=True
+    )
+    
+def run_command(config: DINOV1Config):
+    conf = OmegaConf.create(config)
+    OmegaConf.save(config=conf, f=os.path.join(default_path, f'{config.train.name}.yaml'))
+    
+    command = ' '.join([
+                        "condor_submit", 
+                        f"wandb_key={os.environ.get('WANDB_API_KEY')}",
+                        f'config_name={config.train.name}.yaml',
+                        f"config_path={os.path.join(default_path, f'{config.train.name}.yaml')}",
+                        "hyperparam_sweep.sh"
+                    ])
+    submit_command(command=command)
+
+def main():    
+    config = DINOV1Config()
+    config.train.name = "channvelvit_starting_baseline"
+    run_command(config)
+
+if __name__ == "__main__":
+    main()
