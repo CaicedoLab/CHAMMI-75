845ee03d45d1404171e5ec2d4daab5a2372a9810
John Peters <jgpeters3@wisc.edu>
2025-09-19 17:52:24 -0500

    chtc files

--- benchmarks/morphem/feature_extraction.py
+++ benchmarks/morphem/feature_extraction.py
@@ -113,7 +113,8 @@ def get_save_features(feature_dir, root_dir, model_check, batch_size):
             all_feat = all_feat.squeeze()
 
         
-        feature_path = feature_path = f'{feature_dir}/{dataset_name}/{feature_file}'
+        feature_path  = f'{feature_dir}/{dataset_name}/pretrained_vit_features.npy'
+        os.makedirs(feature_path, exist_ok=True)
         np.save(feature_path, all_feat)
         torch.cuda.empty_cache() # new line
         
